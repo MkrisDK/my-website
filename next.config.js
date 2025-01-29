4d70843e7e2be844e603d443f05dcaa925d37313
MkrisDK <mkrisprivat@gmail.com>
2025-01-29 15:56:53 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  distDir: '.next'  // Dette fortæller Next.js hvor den skal outputte build filer
+  output: 'export',
+  distDir: 'build'
 };
 
 module.exports = nextConfig;
